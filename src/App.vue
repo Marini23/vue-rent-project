be10a8c05a7d59643bad84ef094b780d789cdcea
Marini23 <marina.smile1m@gmail.com>
2024-02-10 00:07:56 +0100

    add_apartmentsList

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,11 @@
 <script setup>
-import ApartmentsItem from "./components/apartment/ApartmentsItem.vue";
-const apartment = {
-  id: "5f689a2c09b16000e9a2f485",
-  title: "Nihil pariatur eum ab a ducimus nam.",
-  imgUrl: "/img/apartments/apartment-8.jpg",
-  descr:
-    "Quae aliquid odit aut quia. Sed aut sed quis repudiandae est consequatur id quo ut. Molestiae nemo maiores quis deleniti sequi et eaque architecto officia. Molestiae incidunt qui quia. Fugiat id eum totam porro autem.",
-  rating: 1,
-  price: 3959,
-  location: {
-    city: "Kharkiv",
-  },
-  owner: {
-    name: "Estell",
-    phone: "804-325-1372",
-    email: "Alta_Green@hotmail.com",
-  },
-};
+import ApartmentsList from "./components/apartment/ApartmentsList.vue";
+import apartments from "./components/apartment/apartments";
 </script>
 
 <template>
   <div id="app">
-    <ApartmentsItem
-      :descr="apartment.descr"
-      :price="apartment.price"
-      :rating="apartment.rating"
-    />
+    <ApartmentsList :items="apartments" />
   </div>
 </template>
 
