860606ff1bdfe048f22f8e75ad7a97ecd8f58dc7
Marini23 <marina.smile1m@gmail.com>
2024-02-09 15:32:04 +0100

    first_component

---
+++ src/components/Button.vue
@@ -0,0 +1,36 @@
+<template>
+  <button v-bind:type="type" :class="{ btn: true, 'btn--outlined': outlined }">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    type: {
+      type: String,
+      default: "button",
+    },
+    outlined: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  display: inline-block;
+  cursor: pointer;
+  min-width: 220px;
+  padding: 8px 15px;
+
+  &--outlined {
+    background: none;
+    color: red;
+    border-color: 1px solid red;
+  }
+}
+</style>
